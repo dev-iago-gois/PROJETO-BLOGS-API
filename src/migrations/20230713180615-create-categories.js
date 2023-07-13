af703ded46e7aa083d3b57cc8a9dbab3c97bce9b
Iago Gois <iagorafaelbg@gmail.com>
2023-07-13 16:00:56 -0300

    feat: Create migrations for the tables users, categories, blog_posts, posts_categories.

---
+++ src/migrations/20230713180615-create-categories.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull:false,
+        type: Sequelize.STRING,
+        // unique: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
