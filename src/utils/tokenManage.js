57acd1219691c2e3340c7f4b361e3eb13c1d55d3
Iago Gois <iagorafaelbg@gmail.com>
2023-07-14 14:49:45 -0300

    refact: changed token expiresIn from 15m to 1d

--- src/utils/tokenManage.js
+++ src/utils/tokenManage.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const jwtConfig = { algorithm: 'HS256', expiresIn: '15m' };
+const jwtConfig = { algorithm: 'HS256', expiresIn: '1d' };
 const { JWT_SECRET } = process.env;
 
 const createToken = (payload) => jwt.sign(payload, JWT_SECRET, jwtConfig);
